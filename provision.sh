22c20437c2e71c12e8cad8e4bc89b2d37806e192
Glen Horton <hortongn@ucmail.uc.edu>
2019-01-12 19:14:39 -0500

    Update provision script

--- provision.sh
+++ provision.sh
@@ -2,7 +2,7 @@
 
 sudo yum update -y
 sudo yum install -y epel-release
-sudo yum install -y lynx mailx git unzip nano vim redis ImageMagick clamav clamav-update clamav-devel mysql-devel java-1.8.0-openjdk-devel nodejs gcc bzip2 kernel-devel dkms libevent-devel libxslt-devel libxml2-devel ghostscript openoffice ntp
+sudo yum install -y wget lynx mailx git unzip nano vim redis ImageMagick clamav clamav-update clamav-devel mysql-devel java-1.8.0-openjdk-devel nodejs gcc bzip2 kernel-devel dkms libevent-devel libxslt-devel libxml2-devel ghostscript openoffice ntp
 
 # Install ffmpeg
 sudo rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
@@ -20,25 +20,20 @@ su - vagrant -c 'cd /home/vagrant/tmux-2.2 && ./configure && make'
 cd /home/vagrant/tmux-2.2 && sudo make install
 
 # Install RVM
-su - vagrant -c 'gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3'
+su - vagrant -c 'gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB'
 su - vagrant -c 'curl -L https://get.rvm.io | bash -s stable --ruby'
-su - vagrant -c 'rvm 2.3.3 --install --default'
+su - vagrant -c 'rvm 2.5.3 --install --default'
 
 # install bundler
 su - vagrant -c 'gem install bundler'
 
 # Install Fits
-su - vagrant -c 'cd /home/vagrant && wget http://projects.iq.harvard.edu/files/fits/files/fits-0.8.5.zip'
-su - vagrant -c 'cd /home/vagrant && unzip fits-0.8.5.zip'
-su - vagrant -c 'chmod u+x /home/vagrant/fits-0.8.5/fits.sh'
-
-# Install phantomjs
-su - vagrant -c 'cd /home/vagrant && wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2'
-su - vagrant -c 'cd /home/vagrant && tar -xvf phantomjs-2.1.1-linux-x86_64.tar.bz2'
-cd /home/vagrant/phantomjs-2.1.1-linux-x86_64/bin && sudo cp phantomjs /usr/bin
+su - vagrant -c 'cd /home/vagrant && wget http://projects.iq.harvard.edu/files/fits/files/fits-1.0.5.zip'
+su - vagrant -c 'cd /home/vagrant && unzip fits-1.0.5.zip'
+su - vagrant -c 'chmod u+x /home/vagrant/fits-1.0.5/fits.sh'
 
 # Install Redis
-su - vagrant -c 'cd /home/vagrant && wget http://download.redis.io/releases/redis-2.8.3.tar.gz'
-su - vagrant -c 'cd /home/vagrant && tar xzvf redis-2.8.3.tar.gz'
-su - vagrant -c 'cd /home/vagrant/redis-2.8.3 && make'
-cd /home/vagrant/redis-2.8.3 && sudo make install
+su - vagrant -c 'cd /home/vagrant && wget http://download.redis.io/releases/redis-4.0.3.tar.gz'
+su - vagrant -c 'cd /home/vagrant && tar xzvf redis-4.0.3.tar.gz'
+su - vagrant -c 'cd /home/vagrant/redis-4.0.3 && make'
+cd /home/vagrant/redis-4.0.3 && sudo make install
